7c7e3722128f60dedc39556321fad6cea2b3041c
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-10-30 11:55:14 -0500

    add function to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+function generatePassword(){
+  var passwordCritaria = alert("Your password should have uppercase, lowercase, numeric, spacecial characters and must be between 8-128 characters.");
+
+  var passwordLength = prompt("How many characters would you like your password to be?");
+while(passwordLength <=7 || passwordLength >= 128){
+  alert("password length must be between 8-128 characters. please try again");
+  var passwordLength = prompt("How many characters would you like your password to be?");
+}
+  // var confirmLength = (prompt("How many characters would like your password to contain?"));
+
+  // 1. prompt the user for the password critaria 
+  //    a. password length 8 < 128
+  //    b. lowercase, uppercase, numbers, special characters
+  // 2. validate the input
+  // 3. generate password 
+  
+  // 4. display password
+  return "Generated password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
