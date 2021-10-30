4e0249599f445ad0a66ad4ccd1020f4f53b33b75
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-10-30 14:11:37 -0500

    updated the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,42 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = LowerCaseLetters.toUpperCase;
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/; 
+
 var passwordLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
+var confirmSpecialChar;
+var confirmNumber;
 var confirmUpperCase;
 var confirmLowerCase;
 
 function generatePassword(){
-  var passwordCritaria = alert("Your password should have uppercase, lowercase, numeric, spacecial characters and must be between 8-128 characters.");
+  var passwordCritaria = alert("Your password should have uppercase, lowercase, numbers, spacecial characters and must be between 8-128 characters.");
 
   var passwordLength = prompt("How many characters would you like your password to be?");
 while(passwordLength <=7 || passwordLength >= 128){
   alert("password length must be between 8-128 characters. please try again");
   var passwordLength = prompt("How many characters would you like your password to be?");
-}
+  }
+
+  var confirmUpperCase = confirm("Click OK if you would like to include uppercase");
+  var confirmLowerCase = confirm("Click OK if you would like to include lowercase");
+  var confirmNumber = confirm("Click OK if you would like to include numbers");
+  var confirmSpecialChar = confirm("Click OK if you would like to include special characters");
+
+
+while (!confirmUpperCase && !confirmLowerCase && !confirmNumber && !confirmSpecialChar) {
+  alert("You must select at least one character type!");
+
+  var confirmUpperCase = confirm("Click OK if you would like to include uppercase");
+  var confirmLowerCase = confirm("Click OK if you would like to include lowercase");
+  var confirmNumber = confirm("Click OK if you would like to include numbers");
+  var confirmSpecialChar = confirm("Click OK if you would like to include special characters");
+  }
+  
+};
   // var confirmLength = (prompt("How many characters would like your password to contain?"));
 
   // 1. prompt the user for the password critaria 
@@ -27,8 +49,8 @@ while(passwordLength <=7 || passwordLength >= 128){
   // 3. generate password 
   
   // 4. display password
-  return "Generated password will go here!";
-}
+  // return "Generated password will go here!";
+
 
 // Write password to the #password input
 function writePassword() {
